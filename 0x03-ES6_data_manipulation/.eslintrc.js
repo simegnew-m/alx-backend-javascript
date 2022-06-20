627a3f7189adc0b364532c94ece34af3a0b73642
simegnew-m <simegnew.misganaw@gmail.com>
2022-06-20 11:16:46 -0700

    # 0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,110 @@
+0x03. ES6 data manipulation
+JavaScript
+ES6
+ By Johann Kerbrat, Engineering Manager at Uber Works
+ Weight: 1
+ Ongoing project - started Jun 20, 2022, must end by Jun 22, 2022 - you're done with 0% of tasks.
+ Checker was released at Jun 20, 2022 6:00 PM
+ An auto review will be launched at the deadline
+
+
+Resources
+Read or watch:
+
+Array
+Typed Array
+Set Data Structure
+Map Data Structure
+WeakMap
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+How to use map, filter and reduce on arrays
+Typed arrays
+The Set, Map, and Weak link data structures
+Requirements
+All your files will be executed on Ubuntu 18.04 LTS using NodeJS 12.11.x
+Allowed editors: vi, vim, emacs, Visual Studio Code
+All your files should end with a new line
+A README.md file, at the root of the folder of the project, is mandatory
+Your code should use the js extension
+Your code will be tested using Jest and the command npm run test
+Your code will be verified against lint using ESLint
+Your code needs to pass all the tests and lint. You can verify the entire project running npm run full-test
+All of your functions must be exported
+Setup
+Install NodeJS 12.11.x
+(in your home directory):
+
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+$ nodejs -v
+v12.11.1
+$ npm -v
+6.11.3
+Install Jest, Babel, and ESLint
+in your project directory:
+
+Install Jest using: npm install --save-dev jest
+Install Babel using: npm install --save-dev babel-jest @babel/core @babel/preset-env
+Install ESLint using: npm install --save-dev eslint
+Configuration files
+package.json
+Click to show/hide file contents
+babel.config.js
+Click to show/hide file contents
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
+
+.eslintrc.js
+Click to show/hide file contents
+
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
